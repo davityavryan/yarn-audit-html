4ea142b9c473c59b3da4f2b55a6bbc7f285fd277
Davit Yavryan <davit.yavryan@tanso.io>
2023-07-30 16:18:59 +0200

    chore: upgrade dependencies and fix code style with new rules

--- src/cli.ts
+++ src/cli.ts
@@ -1,157 +1,157 @@
 #!/usr/bin/env node
 
-import { fstatSync } from 'node:fs';
-import { spawnSync } from 'node:child_process';
-import process from 'node:process';
+import { fstatSync } from "node:fs";
+import { spawnSync } from "node:child_process";
+import process from "node:process";
 
-import { program, Option } from 'commander';
+import { program, Option } from "commander";
 
-import { bailWithError, generateReport, parseAdvisory } from './index.js';
+import { bailWithError, generateReport, parseAdvisory } from "./index.js";
 import {
-    AuditMetadata,
-    RawAuditAdvisor,
-    AuditAdvisoryData,
-    Options,
-    AuditMetadataData,
-    AuditAdvisor,
-} from './types.js';
+	AuditMetadata,
+	RawAuditAdvisor,
+	AuditAdvisoryData,
+	Options,
+	AuditMetadataData,
+	AuditAdvisor,
+} from "./types.js";
 
-async function run(argv: NodeJS.Process['argv'], input: typeof process.stdin) {
-    program
-        .option('-o, --output [output path]', 'output file', 'yarn-audit.html')
-        .option(
-            '-t, --template [ejs path]',
-            'ejs template path',
-            new URL('../templates/template.ejs', import.meta.url).pathname
-        )
-        .addOption(
-            new Option('--theme [theme name]', 'Bootswatch theme. see https://bootswatch.com/#:~:text=Cerulean')
-                .default('materia')
-                .choices([
-                    'cerulean',
-                    'cosmo',
-                    'cyborg',
-                    'darkly',
-                    'flatly',
-                    'journal',
-                    'litera',
-                    'lumen',
-                    'lux',
-                    'materia',
-                    'minty',
-                    'morph',
-                    'pulse',
-                    'quartz',
-                    'sandstone',
-                    'simplex',
-                    'sketchy',
-                    'slate',
-                    'solar',
-                    'spacelab',
-                    'superhero',
-                    'united',
-                    'vapor',
-                    'yeti',
-                    'zephyr',
-                ])
-        )
-        .option('--fatal-exit-code', 'exit with code 1 if vulnerabilities were found', false)
-        .parse(argv);
+async function run(argv: NodeJS.Process["argv"], input: typeof process.stdin) {
+	program
+		.option("-o, --output [output path]", "output file", "yarn-audit.html")
+		.option(
+			"-t, --template [ejs path]",
+			"ejs template path",
+			new URL("../templates/template.ejs", import.meta.url).pathname,
+		)
+		.addOption(
+			new Option("--theme [theme name]", "Bootswatch theme. see https://bootswatch.com/#:~:text=Cerulean")
+				.default("materia")
+				.choices([
+					"cerulean",
+					"cosmo",
+					"cyborg",
+					"darkly",
+					"flatly",
+					"journal",
+					"litera",
+					"lumen",
+					"lux",
+					"materia",
+					"minty",
+					"morph",
+					"pulse",
+					"quartz",
+					"sandstone",
+					"simplex",
+					"sketchy",
+					"slate",
+					"solar",
+					"spacelab",
+					"superhero",
+					"united",
+					"vapor",
+					"yeti",
+					"zephyr",
+				]),
+		)
+		.option("--fatal-exit-code", "exit with code 1 if vulnerabilities were found", false)
+		.parse(argv);
 
-    console.info('Checking audit logs...');
+	console.info("Checking audit logs...");
 
-    let summary: AuditMetadata = {
-        vulnerabilities: { info: 0, low: 0, moderate: 0, high: 0, critical: 0 },
-        dependencies: 0,
-        devDependencies: 0,
-        optionalDependencies: 0,
-        totalDependencies: 0,
-    };
-    const options = program.opts<Options>();
-    const vulnerabilities = new Map<string, AuditAdvisor>();
+	let summary: AuditMetadata = {
+		vulnerabilities: { info: 0, low: 0, moderate: 0, high: 0, critical: 0 },
+		dependencies: 0,
+		devDependencies: 0,
+		optionalDependencies: 0,
+		totalDependencies: 0,
+	};
+	const options = program.opts<Options>();
+	const vulnerabilities = new Map<string, AuditAdvisor>();
 
-    const { stdout } = spawnSync('yarn', ['--version']);
+	const { stdout } = spawnSync("yarn", ["--version"]);
 
-    const yarnMajorVersion = Number.parseInt(stdout.toString());
+	const yarnMajorVersion = Number.parseInt(stdout.toString());
 
-    // Determine if cli is piped *in*
-    const stats = fstatSync(0);
+	// Determine if cli is piped *in*
+	const stats = fstatSync(0);
 
-    if (!stats.isFIFO()) {
-        program.outputHelp();
-        return process.exit(1);
-    }
+	if (!stats.isFIFO()) {
+		program.outputHelp();
+		return process.exit(1);
+	}
 
-    let text = '';
-    input.on('readable', function (this: typeof process.stdin) {
-        try {
-            const chunk = this.read();
+	let text = "";
+	input.on("readable", function (this: typeof process.stdin) {
+		try {
+			const chunk = this.read();
 
-            if (chunk !== null) {
-                text += chunk;
+			if (chunk !== null) {
+				text += chunk;
 
-                const lines = text.split('\n');
+				const lines = text.split("\n");
 
-                if (lines.length > 1) {
-                    text = lines.splice(-1, 1)[0];
+				if (lines.length > 1) {
+					text = lines.splice(-1, 1)[0];
 
-                    lines.forEach((line) => {
-                        if (yarnMajorVersion >= 2) {
-                            const auditAdvisoryData = JSON.parse(line) as {
-                                advisories: Record<string, RawAuditAdvisor>;
-                                metadata: AuditMetadata;
-                            };
+					lines.forEach((line) => {
+						if (yarnMajorVersion >= 2) {
+							const auditAdvisoryData = JSON.parse(line) as {
+								advisories: Record<string, RawAuditAdvisor>;
+								metadata: AuditMetadata;
+							};
 
-                            Object.values(auditAdvisoryData.advisories).forEach((rawAdvisory) => {
-                                advisoryToVulnerabilities(rawAdvisory);
-                            });
+							Object.values(auditAdvisoryData.advisories).forEach((rawAdvisory) => {
+								advisoryToVulnerabilities(rawAdvisory);
+							});
 
-                            summary = auditAdvisoryData.metadata;
-                        } else {
-                            const auditAdvisoryData = JSON.parse(line) as AuditAdvisoryData | AuditMetadataData;
+							summary = auditAdvisoryData.metadata;
+						} else {
+							const auditAdvisoryData = JSON.parse(line) as AuditAdvisoryData | AuditMetadataData;
 
-                            if (auditAdvisoryData.type === 'auditAdvisory') {
-                                advisoryToVulnerabilities(auditAdvisoryData.data.advisory);
-                            } else if (auditAdvisoryData.type === 'auditSummary') {
-                                summary = auditAdvisoryData.data;
-                            }
-                        }
-                    });
-                }
-            }
-        } catch (error) {
-            bailWithError('Failed to parse YARN Audit JSON!', error as Error, options.fatalExitCode);
-        }
-    });
+							if (auditAdvisoryData.type === "auditAdvisory") {
+								advisoryToVulnerabilities(auditAdvisoryData.data.advisory);
+							} else if (auditAdvisoryData.type === "auditSummary") {
+								summary = auditAdvisoryData.data;
+							}
+						}
+					});
+				}
+			}
+		} catch (error) {
+			bailWithError("Failed to parse YARN Audit JSON!", error as Error, options.fatalExitCode);
+		}
+	});
 
-    input.on('end', async function () {
-        try {
-            await generateReport(Array.from(vulnerabilities.values()), summary, options);
-        } catch (error) {
-            bailWithError(
-                'Failed to generate report! Please report this issue to https://github.com/davityavryan/yarn-audit-html/issues',
-                error as Error,
-                options.fatalExitCode
-            );
-        }
-    });
+	input.on("end", async function () {
+		try {
+			await generateReport(Array.from(vulnerabilities.values()), summary, options);
+		} catch (error) {
+			bailWithError(
+				"Failed to generate report! Please report this issue to https://github.com/davityavryan/yarn-audit-html/issues",
+				error as Error,
+				options.fatalExitCode,
+			);
+		}
+	});
 
-    function advisoryToVulnerabilities(advisory: RawAuditAdvisor) {
-        const newVulnerabilities = parseAdvisory(advisory);
+	function advisoryToVulnerabilities(advisory: RawAuditAdvisor) {
+		const newVulnerabilities = parseAdvisory(advisory);
 
-        newVulnerabilities.forEach((newVulnerability) => {
-            const { key } = newVulnerability;
+		newVulnerabilities.forEach((newVulnerability) => {
+			const { key } = newVulnerability;
 
-            if (!vulnerabilities.has(key)) {
-                vulnerabilities.set(key, newVulnerability);
-            }
-        });
-    }
+			if (!vulnerabilities.has(key)) {
+				vulnerabilities.set(key, newVulnerability);
+			}
+		});
+	}
 }
 
 /* istanbul ignore if */
-if (process.env.NODE_ENV !== 'test') {
-    run(process.argv, process.stdin);
+if (process.env.NODE_ENV !== "test") {
+	run(process.argv, process.stdin);
 }
 
 export { run };
--- src/index.ts
+++ src/index.ts
@@ -1,103 +1,103 @@
-import { readFile, writeFile } from 'node:fs/promises';
-import process from 'node:process';
+import { readFile, writeFile } from "node:fs/promises";
+import process from "node:process";
 
-import ejs from 'ejs';
-import { marked } from 'marked';
+import ejs from "ejs";
+import { marked } from "marked";
 
-import { AuditAdvisor, AuditMetadata, Options, RawAuditAdvisor, Severity, Vulnerabilities } from './types.js';
+import { AuditAdvisor, AuditMetadata, Options, RawAuditAdvisor, Severity, Vulnerabilities } from "./types.js";
 
 const bootstrapClassSeverityMap: Record<Severity, string> = {
-    critical: 'danger',
-    high: 'warning',
-    moderate: 'info',
-    low: 'primary',
-    info: 'secondary',
+	critical: "danger",
+	high: "warning",
+	moderate: "info",
+	low: "primary",
+	info: "secondary",
 };
 const severitySortPriority = Object.keys(bootstrapClassSeverityMap);
 
 export function parseAdvisory(advisory: RawAuditAdvisor) {
-    const vulnerabilities: Vulnerabilities = {};
+	const vulnerabilities: Vulnerabilities = {};
 
-    advisory.findings.forEach((finding) => {
-        const { version } = finding;
-        const key = `${advisory.module_name}@${version}-${advisory.vulnerable_versions}-${advisory.created}.${advisory.cwe}`;
+	advisory.findings.forEach((finding) => {
+		const { version } = finding;
+		const key = `${advisory.module_name}@${version}-${advisory.vulnerable_versions}-${advisory.created}.${advisory.cwe}`;
 
-        if (!(key in vulnerabilities)) {
-            vulnerabilities[key] = {
-                ...advisory,
-                key,
-                version,
-                paths: finding.paths,
-            };
-        } else {
-            vulnerabilities[key].paths = [...vulnerabilities[key].paths, ...finding.paths];
-        }
-    });
+		if (!(key in vulnerabilities)) {
+			vulnerabilities[key] = {
+				...advisory,
+				key,
+				version,
+				paths: finding.paths,
+			};
+		} else {
+			vulnerabilities[key].paths = [...vulnerabilities[key].paths, ...finding.paths];
+		}
+	});
 
-    Object.entries(vulnerabilities).forEach(([key, vulnerability]) => {
-        vulnerabilities[key].paths = Array.from(new Set(vulnerability.paths));
-    });
+	Object.entries(vulnerabilities).forEach(([key, vulnerability]) => {
+		vulnerabilities[key].paths = Array.from(new Set(vulnerability.paths));
+	});
 
-    return Object.values(vulnerabilities);
+	return Object.values(vulnerabilities);
 }
 
 export async function generateReport(vulnerabilities: AuditAdvisor[], summary: AuditMetadata, options: Options) {
-    vulnerabilities.sort(
-        (left, right) => severitySortPriority.indexOf(left.severity) - severitySortPriority.indexOf(right.severity)
-    );
+	vulnerabilities.sort(
+		(left, right) => severitySortPriority.indexOf(left.severity) - severitySortPriority.indexOf(right.severity),
+	);
 
-    const report = await renderReport(
-        {
-            reportDate: new Date(),
-            vulnerabilities,
-            theme: options.theme,
-            summary: {
-                vulnerabilities: Object.values(summary.vulnerabilities).reduce((sum, value) => sum + value, 0),
-                totalDependencies: summary.totalDependencies,
-            },
-        },
-        options.template
-    );
+	const report = await renderReport(
+		{
+			reportDate: new Date(),
+			vulnerabilities,
+			theme: options.theme,
+			summary: {
+				vulnerabilities: Object.values(summary.vulnerabilities).reduce((sum, value) => sum + value, 0),
+				totalDependencies: summary.totalDependencies,
+			},
+		},
+		options.template,
+	);
 
-    await writeReport(options.output, report);
+	await writeReport(options.output, report);
 
-    if (vulnerabilities.length > 0) {
-        console.info(`Found ${vulnerabilities.length} vulnerabilities. Report is saved in "${options.output}"`);
+	if (vulnerabilities.length > 0) {
+		console.info(`Found ${vulnerabilities.length} vulnerabilities. Report is saved in "${options.output}"`);
 
-        if (options.fatalExitCode) {
-            return process.exit(1);
-        }
-    } else {
-        console.info('Congrats!!! No vulnerabilities found.');
-    }
+		if (options.fatalExitCode) {
+			return process.exit(1);
+		}
+	} else {
+		console.info("Congrats!!! No vulnerabilities found.");
+	}
 
-    return process.exit(0);
+	return process.exit(0);
 }
 
 export async function renderReport(data: ejs.Data, template: string) {
-    const htmlTemplate = await readFile(template, 'utf8');
-    const locale = Intl.DateTimeFormat().resolvedOptions().locale;
+	const htmlTemplate = await readFile(template, "utf8");
+	const locale = Intl.DateTimeFormat().resolvedOptions().locale;
 
-    return ejs.render(htmlTemplate, {
-        data,
-        formatNumber: (number: number) => new Intl.NumberFormat(locale).format(number),
-        formatDate: (dateStr: string) =>
-            new Intl.DateTimeFormat(locale, { dateStyle: 'long', timeStyle: 'long' }).format(new Date(dateStr)),
-        severityClass: (severity: Severity) => bootstrapClassSeverityMap[severity],
-        markdown: (code: string) => marked(code, { mangle: false, headerIds: false }),
-    });
+	return ejs.render(htmlTemplate, {
+		data,
+		formatNumber: (number: number) => new Intl.NumberFormat(locale).format(number),
+		formatDate: (dateStr: string) =>
+			new Intl.DateTimeFormat(locale, { dateStyle: "long", timeStyle: "long" }).format(new Date(dateStr)),
+		severityClass: (severity: Severity) => bootstrapClassSeverityMap[severity],
+		markdown: (code: string) => marked(code, { mangle: false, headerIds: false }),
+	});
 }
 
 export async function writeReport(outputPath: string, report: string) {
-    await writeFile(outputPath, report, { encoding: 'utf8' });
+	await writeFile(outputPath, report, { encoding: "utf8" });
 }
 
 export function bailWithError(message: string, error: Error, isFatalExitCode: boolean) {
-    console.error(`${message}\n`, error);
+	console.error(`${message}\n`, error);
 
-    if (isFatalExitCode) {
-        return process.exit(1);
-    }
+	if (isFatalExitCode) {
+		return process.exit(1);
+	}
 
-    return process.exit(0);
+	return process.exit(0);
 }
